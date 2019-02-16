15264e338571de7955c0c33129eab796b1c923e3
winters23 <jain.diksha2398@gmail.com>
2019-02-16 13:51:07 +0530

    program of advance calculator

---
+++ adv_calc.py
@@ -0,0 +1,26 @@
+import re
+
+print("our magical calculator")
+print("Type 'quit' to exit \n")
+previous =0
+run = True
+def performMath():
+    global run
+    global previous
+    equation = " "
+    if previous==0:
+        equation = input("enter equation: ")
+    else:
+        equation = input(str(previous))
+    if equation == 'quit':
+        print("good bye,human")
+        run = False
+    else:
+        equation = re.sub('[a-zA-Z,.:()""]','',equation)
+        if previous == 0:
+            previous = eval (equation)
+        else:
+            previous=eval(str(previous)+equation)
+
+while run:
+    performMath()
