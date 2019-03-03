d5da653e2a26317ceb83760d677104300b6b6638
winters23 <jain.diksha2398@gmail.com>
2019-03-03 14:26:49 +0530

    tutle moves

---
+++ turtle.py
@@ -0,0 +1,15 @@
+import turtle
+wn=turtle.screen
+wn.setup(500,440)
+circle_rad=60
+rectangle_width=240
+rectangle_height=30
+alex=turtle.Turtle()
+alex.shape("turtle")
+alex.color("red")
+alex.backward(rectangle_width/2)
+alex.forward(rectangle_width)
+alex.right(90)
+alex.forward(rectangle_width)
+alex.right(90)
+alex.forward(rectangle_height)
