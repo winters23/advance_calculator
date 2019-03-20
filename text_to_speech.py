843f9e36c7318cc46b02a0d8ec2efec50a5de84e
winters23 <jain.diksha2398@gmail.com>
2019-03-20 14:01:29 +0530

    text to speech convertor

---
+++ text_to_speech.py
@@ -0,0 +1,5 @@
+import pyttsx3
+engine = pyttsx3.init()
+engine.say('bitty bought some butter but butter is bitter')
+engine.say('so bitty bought some more butter to make bitter butter better')
+engine.runAndWait()
