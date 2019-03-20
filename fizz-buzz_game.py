e9da819534ee1a0876cf614793dc1c897ea73944
winters23 <jain.diksha2398@gmail.com>
2019-03-20 13:42:47 +0530

    print fizz if divisible by 3, buzz if divisible by 5 and fizz buzz if divisible by both 3 and 5

--- fizz-buzz_game.py
+++ fizz-buzz_game.py
@@ -1,9 +1,9 @@
 for i in range(1,51):
-    if(i%3==0):
+    if(i%3==0 and i%5==0):
+        print("fizzbuzz")
+    elif(i%3==0):
         print("fizz")
     elif(i%5==0):
         print("buzz")
-    elif(i%3==0 and i%5==0):
-        print("fizzbuzz")
     else:
         print(i)
