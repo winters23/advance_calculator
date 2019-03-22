2b425ac998988af8c156d3bfae7fb178f5b8a82a
winters23 <jain.diksha2398@gmail.com>
2019-03-22 13:05:30 +0530

    convert the numbers entered in text form

---
+++ numtoword.py
@@ -0,0 +1,4 @@
+str=input("enter a number:")
+dic={'0':"zero",'1':"one",'2':"two",'3':"three",'4':"four",'5':"five",'6':"six",'7':"seven",'8':"eight",'9':"nine"}
+for i in str:
+    print(dic[i],end=" ")
